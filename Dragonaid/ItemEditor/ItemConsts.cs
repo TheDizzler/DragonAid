37958b8e31c0d4e4da4640e0ac139f497b56ce65
AtomosZ <tgillon.sdk@gmail.com>
2022-04-01 00:10:08 +0900

    Item Editor

---
+++ Dragonaid/ItemEditor/ItemConsts.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Generic;
+
+namespace AtomosZ.Dragonaid.ItemEditor
+{
+	public static class ItemConsts
+	{
+		public static int WeaponPowers = 0x027990;
+		public static int ArmorPowers = 0x0279B0;
+		public static int ShieldPowers = 0x0279C8;
+		public static int HelmetPowers = 0x0279CF;
+
+
+		public static List<string> itemBasicList = new List<string>()
+		{
+			"00 - Cypress stick",
+			"01 - Club",
+			"02 - Copper sword",
+			"03 - Magic Knife",
+			"04 - Iron Spear",
+			"05 - Battle Axe",
+			"06 - Broad Sword",
+			"07 - Wizard's Wand",
+			"08 - Poison Needle",
+			"09 - Iron Claw",
+			"0A - Thorn Whip",
+			"0B - Giant Shears",
+			"0C - Chain Sickle",
+			"0D - Thor's Sword",
+			"0E - Snowblast Sword",
+			"0F - Demon Axe",
+			"10 - Staff of Rain",
+			"11 - Sword of Gaia",
+			"12 - Staff of Reflection",
+			"13 - Sword of Destruction",
+			"14 - Multi-Edge Sword",
+			"15 - Staff of Force",
+			"16 - Sword of Illusion",
+			"17 - Zombie Slasher",
+			"18 - Falcon Sword",
+			"19 - Sledge Hammer",
+			"1A - Thunder Sword",
+			"1B - Staff of Thunder",
+			"1C - Sword of Kings",
+			"1D - Orochi Sword",
+			"1E - Dragon Killer",
+			"1F - Staff of Judgement",
+
+			"20 - Clothes",
+			"21 - Training Suit",
+			"22 - Leather Armor",
+			"23 - Flashy Clothes",
+			"24 - Half Plate Armor",
+			"25 - Full Plate Armor",
+			"26 - Magic Armor",
+			"27 - Cloak of Evasion",
+			"28 - Armor of Radiance",
+			"29 - Iron Apron",
+			"2A - Animal Suit",
+			"2B - Fightting Suit",
+			"2C - Sacred Robe",
+			"2D - Armor of Hades",
+			"2E - Water Flying Cloth",
+			"2F - Chain Mail",
+			"30 - Wayfarers Clothes",
+			"31 - Revealing Swimsuit",
+			"32 - Magic Bikini",
+			"33 - Shell Armor",
+			"34 - Armor of Terrafirma",
+			"35 - Dragon Mail",
+			"36 - Swordedge Armor",
+			"37 - Angel's Robe",
+
+			"38 - Leather Shield",
+			"39 - Iron Shield",
+			"3A - Shield of Strength",
+			"3B - Shield of Heroes",
+			"3C - Shield of Sorrow",
+			"3D - Bronze Shield",
+			"3E - Silver Shield",
+
+			"3F - Golden Crown",
+			"40 - Iron Helmet",
+			"41 - Mysterious Hat",
+			"42 - Unlucky Helmet",
+			"43 - Turban",
+			"44 - Noh Mask",
+			"45 - Leather Helmet",
+			"46 - Iron Mask",
+
+			"47 - Sacred Amulet",
+			"48 - Ring of Life",
+			"49 - Shoes of Happiness",
+			"4A - Golden Claw",
+			"4B - Meteorite Armband",
+			"4C - Book of Satori",
+
+			"4D - '   '",
+
+			"4E - Wizard's Ring",
+			"4F - Black Pepper",
+			"50 - Sage's Stone",
+			"51 - Mirror of Ra",
+			"52 - Vase of Draught",
+			"53 - Lamp of Darkness",
+			"54 - Staff of Change",
+			"55 - Stone of Life",
+			"56 - Invisibility Herb",
+			"57 - Magic Ball",
+
+			"58 - Thief's Key",
+			"59 - Magic Key",
+			"5A - Final Key",
+
+			"5B - Dream Ruby",
+			"5C - Wake-up Powder",
+			"5D - Royal Scroll",
+			"5E - Oricon",
+
+			"5F - Strength Seed",
+			"60 - Agility Seed",
+			"61 - Vitality Seed",
+			"62 - Luck Seed",
+			"63 - Intelligence Seed",
+			"64 - Acorns of Life",
+
+			"65 - Medical Herb",
+			"66 - Antidote Herb",
+			"67 - Fairy Water",
+			"68 - Wing of Wyvern",
+			"69 - Leaf of the World Tree",
+
+			"6A - '   '",
+
+			"6B - Locket of Love",
+			"6C - Full Moon Herb",
+			"6D - Water Blaster",
+			"6E - Sailors Thigh Bone",
+			"6F - Echoing Flute",
+			"70 - Fairy Flute",
+			"71 - Silver Harp",
+			"72 - Sphere of Light",
+			"73 - Poison Moth Powder",
+			"74 - Spiders Web",
+			"75 - Stone of Sunlight",
+
+			"76 - Rainbow Drop",
+			"77 - Silver Orb",
+			"78 - Red Orb",
+			"79 - Yellow Orb",
+			"7A - Purple Orb",
+			"7B - Blue Orb",
+			"7C - Green Orb",
+
+			"7D - Stick Slime",
+			"7E - Black Raven",
+			"7F - Sword Horned",
+			"FF - Nothing",
+		};
+	}
+}
