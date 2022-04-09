10bcf6f91c503d57b9a0322474c412ec90bfeebf
AtomosZ <tgillon.sdk@gmail.com>
2022-04-09 13:37:00 +0900

    ASM helper for common 6502 ASM operations

---
+++ Dragonaid/DragonaidLibrary/ASMHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace AtomosZ.Dragonaid.Libraries
+{
+	public static class ASMHelper
+	{
+		public static byte ROL(byte operand, int iterations, ref bool carrySet)
+		{
+			for (int i = 0; i < iterations; ++i)
+			{
+				bool hasCarry = (operand & 0x80) == 0x80;
+				operand <<= 1;
+				if (carrySet)
+					operand += 1;
+				carrySet = hasCarry;
+			}
+
+			return operand;
+		}
+
+		public static byte ROR(byte operand, int iterations, ref bool carrySet)
+		{
+			for (int i = 0; i < iterations; ++i)
+			{
+				bool hasCarry = (operand & 0x01) == 0x01;
+				operand >>= 1;
+				if (carrySet)
+					operand += 0x80;
+				carrySet = hasCarry;
+			}
+
+			return operand;
+		}
+	}
+}
