01c235e1529d0bd2c2ae20646e42a7e4bff0ddff
AtomosZ <tgillon.sdk@gmail.com>
2022-05-04 10:36:21 +0900

    method required for address search

--- Dragonaid/DragonaidLibrary/DynamicPointerData.cs
+++ Dragonaid/DragonaidLibrary/DynamicPointerData.cs
@@ -10,5 +10,17 @@ namespace AtomosZ.DragonAid.Libraries
 	{
 		public List<DynamicSubroutine> subroutines07;
 		public List<DynamicSubroutine> subroutines17;
+
+		public List<DynamicSubroutine> FindAddress(int address)
+		{
+			var finds = new List<DynamicSubroutine>();
+			foreach (var addr in subroutines07)
+				if (addr.prgAddress.pointer == address)
+					finds.Add(addr);
+			foreach (var addr in subroutines17)
+				if (addr.prgAddress.pointer == address)
+					finds.Add(addr);
+			return finds;
+		}
 	}
 }
