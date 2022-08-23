edf8f1b4ca8e1f1e21cf98acc857bd706d0f1436
AtomosZ <tgillon.sdk@gmail.com>
2022-08-23 13:37:26 +0900

    cheap and ugly numberbox - will replace

---
+++ Dragonaid/DragonaidLibrary/NumberBox.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AtomosZ.DragonAid.Libraries
+{
+	partial class NumberBox
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// numericUpDown
+			// 
+			this.numericUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.numericUpDown.Location = new System.Drawing.Point(0, 0);
+			this.numericUpDown.Name = "numericUpDown";
+			this.numericUpDown.Size = new System.Drawing.Size(106, 26);
+			this.numericUpDown.TabIndex = 0;
+			// 
+			// NumberBox
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.numericUpDown);
+			this.Name = "NumberBox";
+			this.Size = new System.Drawing.Size(83, 26);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		public System.Windows.Forms.NumericUpDown numericUpDown;
+	}
+}
---
+++ Dragonaid/DragonaidLibrary/NumberBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtomosZ.DragonAid.Libraries
+{
+	public partial class NumberBox : UserControl
+	{
+		public NumberBox()
+		{
+			InitializeComponent();
+			numericUpDown.Controls.RemoveAt(0);
+		}
+
+
+		public bool Hexadecimal
+		{
+			get { return numericUpDown.Hexadecimal; }
+			set { numericUpDown.Hexadecimal = value; }
+		}
+
+		public decimal Maximum
+		{
+			get { return numericUpDown.Maximum; }
+			set { numericUpDown.Maximum = value; }
+		}
+
+		public decimal Minimum
+		{
+			get { return numericUpDown.Minimum; }
+			set { numericUpDown.Minimum = value; }
+		}
+
+		public decimal Increment
+		{
+			get { return numericUpDown.Increment; }
+			set { numericUpDown.Increment = value; }
+		}
+
+		public int Value
+		{
+			get { return (int)numericUpDown.Value; }
+			set { numericUpDown.Value = value; }
+		}
+	}
+}
