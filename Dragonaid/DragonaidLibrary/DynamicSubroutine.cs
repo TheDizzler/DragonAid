eed971c028bba0e1318c9605e90a27189b351e21
AtomosZ <tgillon.sdk@gmail.com>
2022-08-23 19:12:14 +0900

    Dynamic Subroutine

--- Dragonaid/DragonaidLibrary/DynamicSubroutine.cs
+++ Dragonaid/DragonaidLibrary/DynamicSubroutine.cs
@@ -1,27 +1,27 @@
 ﻿namespace AtomosZ.DragonAid.Libraries
 {
-	public enum DynamicLoader { Load07, Load17, LocalPointers, NoLoader }
+	public enum DynamicLoader { Load07, Load17, NoLoader }
 
 	public class DynamicSubroutine
 	{
 		public readonly static Address[] DynamicSubroutineBankPointers = new Address[]
 		{
-			new Address("DynamicSubroutine_Addresses_00000", 0x00000),
-			new Address("DynamicSubroutine_Addresses_04000", 0x04000),
-			new Address("DynamicSubroutine_Addresses_08000", 0x08000),
-			new Address("DynamicSubroutine_Addresses_0C000", 0x0C000), // only 17
-			new Address("DynamicSubroutine_Addresses_10000", 0x10000),
-			new Address("DynamicSubroutine_Addresses_14000", 0x14000),
-			new Address("DynamicSubroutine_Addresses_18000", 0x18000),
-			new Address("DynamicSubroutine_Addresses_1C000", 0x1C000),
-			new Address("DynamicSubroutine_Addresses_20000", 0x20000),
-			new Address("DynamicSubroutine_Addresses_24000", 0x24000), // only 17
-			new Address("DynamicSubroutine_Addresses_28000", 0x28000),
-			new Address("DynamicSubroutine_Addresses_2C000", 0x2C000), // only 17
-			new Address("DynamicSubroutine_Addresses_30000", 0x30000),
-			new Address("DynamicSubroutine_Addresses_34000", 0x34000),
-			new Address("DynamicSubroutine_Addresses_38000", 0x38000),
-			new Address("DynamicSubroutine_Addresses_3C000", 0x3C000),
+			new Address("DynamicSubroutine_Addresses_00000", 0x00000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_04000", 0x04000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_08000", 0x08000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_0C000", 0x0C000, 0x4000), // only 17
+			new Address("DynamicSubroutine_Addresses_10000", 0x10000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_14000", 0x14000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_18000", 0x18000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_1C000", 0x1C000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_20000", 0x20000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_24000", 0x24000, 0x4000), // only 17
+			new Address("DynamicSubroutine_Addresses_28000", 0x28000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_2C000", 0x2C000, 0x4000), // only 17
+			new Address("DynamicSubroutine_Addresses_30000", 0x30000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_34000", 0x34000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_38000", 0x38000, 0x4000),
+			new Address("DynamicSubroutine_Addresses_3C000", 0x3C000, 0x4000),
 		};
 
 		public int code { get; set; }
