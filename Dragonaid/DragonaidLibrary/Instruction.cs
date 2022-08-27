b013190178488e40aa63f26b16ec119fca2f3dd1
AtomosZ <tgillon.sdk@gmail.com>
2022-08-27 20:02:29 +0900

    moved Instruction class to own file

---
+++ Dragonaid/DragonaidLibrary/Instruction.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomosZ.DragonAid.Libraries.ASM
+{
+	/// <summary>
+	/// This data structure class is for debugging purposes and could be stream-lined away eventually.
+	/// However it's very useful for peeking into the next instruction the CPU is about to execute.
+	/// </summary>
+	public class Instruction
+	{
+		public Opcode opcode;
+		public byte[] operands;
+		public int address;
+
+
+		/// <summary>
+		/// Get operand bytes in High-Low order.
+		/// </summary>
+		/// <returns></returns>
+		public string GetBytesString()
+		{
+			var text = "$";
+			for (int i = operands.Length - 1; i >= 0; --i)
+				text += operands[i].ToString("X2");
+			return text;
+		}
+
+		public int GetPointer()
+		{
+			return operands[0] + (operands[1] << 8);
+		}
+
+		public string byteCode
+		{
+			get
+			{
+				string code = opcode.opc.ToString("X2");
+				foreach (var oper in operands)
+					code += " " + oper.ToString("X2");
+				return code;
+			}
+		}
+
+		/// <summary>
+		/// ASM reading of instruction
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			string t = opcode.asm.Substring(0, 3);
+			switch (opcode.mode)
+			{
+				case Opcode.Mode.Accumulator:
+					t += " A";
+					break;
+
+				case Opcode.Mode.Absolute:
+				case Opcode.Mode.ZeroPage:
+					t += $" {GetBytesString()}";
+					break;
+
+				case Opcode.Mode.Indirect:
+					t += $" ({GetBytesString()})";
+					break;
+				case Opcode.Mode.Immediate:
+					t += $" #{GetBytesString()}";
+					break;
+
+				case Opcode.Mode.Absolute_X:
+				case Opcode.Mode.ZeroPage_X:
+					t += $" {GetBytesString()},X";
+					break;
+				case Opcode.Mode.Absolute_Y:
+					t += $" {GetBytesString()},Y";
+					break;
+
+				case Opcode.Mode.Indirect_X:
+					t += $" ({GetBytesString()},X)";
+					break;
+				case Opcode.Mode.Indirect_Y:
+					t += $" ({GetBytesString()}),Y";
+					break;
+
+				case Opcode.Mode.Relative:
+					t += $" ${GetRelativeAddressString()}";
+					break;
+
+				case Opcode.Mode.Implied:
+					break; // nothing to see here
+			}
+
+			return t;
+		}
+
+		public string GetRelativeAddressString()
+		{
+			if (operands[0] >= 0x80)
+			{
+				var i = 0x100 - operands[0];
+				return (address - i).ToString("X4");
+			}
+			else
+				return (address + operands[0]).ToString("X4");
+		}
+
+		public int GetRelativeAddress()
+		{
+			if (operands[0] >= 0x80)
+			{
+				var i = 0x100 - operands[0];
+				return address - i;
+			}
+			else
+				return address + operands[0];
+		}
+	}
+}
--- Dragonaid/DragonaidLibrary/Opcodes.cs
+++ Dragonaid/DragonaidLibrary/Opcodes.cs
@@ -603,98 +603,21 @@ namespace AtomosZ.DragonAid.Libraries
 					break;
 			}
 		}
-	}
 
-	/// <summary>
-	/// This data structure class is for debugging purposes and could be stream-lined away eventually.
-	/// However it's very useful for peeking into the next instruction the CPU is about to execute.
-	/// </summary>
-	public class Instruction
-	{
-		public Opcode opcode;
-		public byte[] operands;
-		public int address;
+		/// <summary>
+		/// </summary>
+		/// <returns></returns>
+		{
+
 
 
 		/// <summary>
-		/// Get bytes in High-Low order.
 		/// </summary>
 		/// <returns></returns>
-		public string GetBytesString()
 		{
-			var text = "$";
-			for (int i = operands.Length - 1; i >= 0; --i)
-				text += operands[i].ToString("X2");
-			return text;
-		}
-
-		public int GetPointer()
-		{
-			return operands[0] + (operands[1] << 8);
-		}
-
-
-		/// <summary>
-		/// ASM reading of instruction
-		/// </summary>
-		/// <returns></returns>
-		public override string ToString()
-		{
-			string t = opcode.asm.Substring(0, 3);
-			switch (opcode.mode)
 			{
-				case Opcode.Mode.Accumulator:
-					t += " A";
-					break;
-
-				case Opcode.Mode.Absolute:
-				case Opcode.Mode.ZeroPage:
-					t += $" {GetBytesString()}";
-					break;
-
-				case Opcode.Mode.Indirect:
-					t += $" ({GetBytesString()})";
-					break;
-				case Opcode.Mode.Immediate:
-					t += $" #{GetBytesString()}";
-					break;
-
-				case Opcode.Mode.Absolute_X:
-				case Opcode.Mode.ZeroPage_X:
-					t += $" {GetBytesString()},X";
-					break;
-				case Opcode.Mode.Absolute_Y:
-					t += $" {GetBytesString()},Y";
-					break;
-
-				case Opcode.Mode.Indirect_X:
-					t += $" ({GetBytesString()},X)";
-					break;
-				case Opcode.Mode.Indirect_Y:
-					t += $" ({GetBytesString()}),Y";
-					break;
-
-				case Opcode.Mode.Relative:
-					t += $" ${GetRelativeAddress()}";
-					break;
-
-				case Opcode.Mode.Implied:
-					break; // nothing to see here
 			}
 
-			return t;
-		}
-
-		private string GetRelativeAddress()
-		{
-			var t = "";
-			if (operands[0] >= 0x80)
-			{
-				var i = 0x100 - operands[0];
-				return (address - i).ToString("X4");
-			}
-			else
-				return (address + operands[0]).ToString("X4");
 		}
 	}
 
