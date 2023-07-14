75da6a480f6b714199ad87a129fdd05d022f8df2
DeckPuncher <tgillon.sdk@gmail.com>
2023-07-15 01:28:07 +0900

    ColorPicker form

---
+++ Dragonaid/DragonaidLibrary/ColorPicker.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace AtomosZ.DragonAid.Libraries
+{
+	partial class ColorPicker
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.palette_tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.SuspendLayout();
+			// 
+			// palette_tableLayoutPanel
+			// 
+			this.palette_tableLayoutPanel.AutoSize = true;
+			this.palette_tableLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.palette_tableLayoutPanel.ColumnCount = 8;
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.palette_tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.palette_tableLayoutPanel.Name = "palette_tableLayoutPanel";
+			this.palette_tableLayoutPanel.RowCount = 8;
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.palette_tableLayoutPanel.Size = new System.Drawing.Size(0, 0);
+			this.palette_tableLayoutPanel.TabIndex = 0;
+			// 
+			// ColorPicker
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(526, 522);
+			this.Controls.Add(this.palette_tableLayoutPanel);
+			this.Name = "ColorPicker";
+			this.Text = "ColorPicker";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel palette_tableLayoutPanel;
+	}
+}
---
+++ Dragonaid/DragonaidLibrary/ColorPicker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtomosZ.DragonAid.Libraries
+{
+	public partial class ColorPicker : Form
+	{
+		public int boxSize = 32;
+		public byte result;
+
+		public ColorPicker()
+		{
+			InitializeComponent();
+			var size = new Size(boxSize, boxSize);
+			for (byte i = 0; i < 4; ++i)
+			{
+				for (byte j = 0; j < 8; ++j)
+				{
+					byte colorByte = (byte)(i * 0x10 + j);
+					var paletteBox = new CaptionedPictureBox();
+					paletteBox.Image = SpriteParser.GetSolidColor(colorByte, size);
+					paletteBox.Size = size;
+					paletteBox.Text = "0x" + (colorByte).ToString("X2");
+					paletteBox.OnMouseClick += OnColorClick;
+					palette_tableLayoutPanel.Controls.Add(paletteBox, j, i);
+				}
+			}
+
+			for (byte i = 0; i < 4; ++i)
+			{
+				for (byte j = 0; j < 8; ++j)
+				{
+					byte colorByte = (byte)(i * 0x10 + (j + 8));
+					var paletteBox = new CaptionedPictureBox();
+					paletteBox.Image = SpriteParser.GetSolidColor(colorByte, size);
+					paletteBox.Size = size;
+					paletteBox.Text = "0x" + (colorByte).ToString("X2");
+					paletteBox.OnMouseClick += OnColorClick;
+					palette_tableLayoutPanel.Controls.Add(paletteBox, j, i + 4);
+				}
+			}
+		}
+
+		private void OnColorClick(object sender, MouseEventArgs e)
+		{
+			var a = sender as PictureBox;
+			if (a != null)
+				Console.WriteLine(a.GetHashCode());
+			var capPicBox = (CaptionedPictureBox)sender;
+			var index = palette_tableLayoutPanel.Controls.GetChildIndex(capPicBox);
+			var colorText = capPicBox.Text.Replace("0x", "");
+			result = Convert.ToByte(colorText, 16);
+
+			DialogResult = DialogResult.OK;
+			this.Close();
+		}
+	}
+}
