829eb6db518a39912806bfa52331e253cfe55b0f
DeckPuncher <tgillon.sdk@gmail.com>
2023-07-15 01:26:33 +0900

    CaptionedPictureBox with OnMouseClick

--- Dragonaid/DragonaidLibrary/CaptionedPictureBox.Designer.cs
+++ Dragonaid/DragonaidLibrary/CaptionedPictureBox.Designer.cs
@@ -40,7 +40,7 @@
 			this.pictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox.InitialImage")));
 			this.pictureBox.Location = new System.Drawing.Point(0, 0);
 			this.pictureBox.Name = "pictureBox";
-			this.pictureBox.Size = new System.Drawing.Size(150, 150);
+			this.pictureBox.Size = new System.Drawing.Size(120, 120);
 			this.pictureBox.TabIndex = 0;
 			this.pictureBox.TabStop = false;
 			// 
@@ -48,9 +48,9 @@
 			// 
 			this.caption_label.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.caption_label.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.caption_label.Location = new System.Drawing.Point(0, 125);
+			this.caption_label.Location = new System.Drawing.Point(0, 95);
 			this.caption_label.Name = "caption_label";
-			this.caption_label.Size = new System.Drawing.Size(150, 25);
+			this.caption_label.Size = new System.Drawing.Size(120, 25);
 			this.caption_label.TabIndex = 1;
 			this.caption_label.Text = "Caption";
 			this.caption_label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -62,6 +62,8 @@
 			this.Controls.Add(this.caption_label);
 			this.Controls.Add(this.pictureBox);
 			this.Name = "CaptionedPictureBox";
+			this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.Size = new System.Drawing.Size(120, 120);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
 			this.ResumeLayout(false);
 
